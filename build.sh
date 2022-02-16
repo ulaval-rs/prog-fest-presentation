8e81d72512b7cf3d700771af2953d9027e3500e7
Gabriel Couture <gacou54@gmail.com>
2022-02-16 16:18:55 -0500

    Ajustement du theme; polissage du contenu
    Trouver une façon de chargé un thème css plutôt que d'installer
    reveal.js au complet.

---
+++ build.sh
@@ -0,0 +1,5 @@
+jupyter nbconvert presentation.ipynb \
+  --to slides --no-prompt \
+  --reveal-prefix reveal.js \
+  --SlidesExporter.reveal_theme=simple-better \
+  --SlidesExporter.reveal_scroll=False
