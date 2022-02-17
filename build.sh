4b7224f8853b23e08dac0d62acd982292167d08d
Gabriel Couture <gacou54@gmail.com>
2022-02-17 10:49:49 -0500

    build script for setting up the theme

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
+if [ -d "./reveal.js" ]
+then
+    echo "reveal.js exists."
+else
+    echo "Downloading reveal.js"
+    git clone https://github.com/hakimel/reveal.js
+fi
+
+cp my-theme.css ./reveal.js/dist/theme/my-theme.css
+
 jupyter nbconvert presentation.ipynb \
   --to slides --no-prompt \
   --reveal-prefix reveal.js \
-  --SlidesExporter.reveal_theme=simple-better \
+  --SlidesExporter.reveal_theme=my-theme \
   --SlidesExporter.reveal_scroll=False
